4b0962a8cd005dcdad07af81345e5d7c558e2c8f
meSajied <sajiedshahyousuf@gmail.com>
2019-06-08 20:53:02 +0600

    MCM completed.....

--- Matrix Chain Multiplicaion.py
+++ Matrix Chain Multiplicaion.py
@@ -1,5 +1,15 @@
 import sys
 
+
+def parenthesize(numMatrices):
+
+    if numMatrices == 1:
+        return
+
+    for k in range(2, numMatrices-1):
+        parenthesize(k)*parenthesize(numMatrices-k)
+
+
 def matrixMultiplyRec(matrix, i, j):
 
     __min = sys.maxsize
@@ -14,8 +24,31 @@ def matrixMultiplyRec(matrix, i, j):
 
     return __min
 
+
 def matrixMultiply(matrix):
 
-    n = len(matrix)-1
-    table1 = [[0 for x in range(n)] for x in range(n)]
-    table2 = [[0 for x in range(n-1)] for x in range(n-1)]
+    n = len(matrix)
+    minCost = [[0 for x in range(n)] for x in range(n)]
+
+    for i in range(1, n):
+        minCost[i][i] = 0
+    for chainLength in range(2, n):
+        for i in range(1, n-chainLength+1):
+            j = i + chainLength - 1
+            minCost[i][j] = sys.maxsize
+            for k in range(i, j):
+                count = minCost[i][k] + minCost[k+1][j] + matrix[i-1]*matrix[k]*matrix[j]
+                if count < minCost[i][j]:
+                    minCost[i][j] = count
+
+    return minCost[i][j]
+
+
+def main():
+
+    arr = [1, 2, 3, 4]
+
+    print(matrixMultiply(arr), matrixMultiplyRec(arr, 1, len(arr)-1))
+
+if __name__ == '__main__':
+    main()
