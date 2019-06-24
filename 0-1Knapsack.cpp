fbb9374b2b1be521c8bed2805b73ab015291ea1f
meSajied <sajiedshahyousuf@gmail.com>
2019-06-24 11:47:55 +0600

    0-1 knapsack updated

--- 0-1Knapsack.cpp
+++ 0-1Knapsack.cpp
@@ -4,23 +4,36 @@ using namespace std;
 typedef struct{
      int Weight, Price;
 }Object;
-int main()
-{
-     int i, w, N, mat[100][100], Capacity;
-     Object Obj[100];
-     cin >> N;
-     for(i = 1; i <= N; i++){
-          cin >> Obj[i].Weight >> Obj[i].Price >> Capacity;
-     }
-     for(i = 1; i <= N; i++){
-          mat[i][0] = 0;
-     }
-     for(i = 1; i <= N; i++)
-          for(w = 1; w <= Capacity; w++){
-               if(Obj[i].Weight > w)
-                    mat[i][w] = mat[i-1][w];
-               else
-                    mat[i][w] = max(mat[i-1][w], mat[i-1][w-Obj[i].Weight]+Obj[i].Price);
-          }
-     cout << mat[N][Capacity] << endl;
+
+int knapSack(int capacity, int weight[], int price[], int n){
+
+	int i, w;
+	int matrix[n+1][capacity+1];
+	//n = sizeof(price)/ sizeof(price[0]);
+
+	for (int i = 0; i <= n; i++) {
+		for (int w = 0; w <= capacity; ++w) {
+
+			if(i == 0 || w == 0)
+				matrix[i][w] = 0;
+			else if(weight[i-1] <= w)
+				matrix[i][w] = max(price[i-1]+matrix[i-1][w-weight[i-1]], matrix[i-1][w]);
+			else
+				matrix[i][w] = matrix[i-1][w];
+
+		}
+
+	}
+	return matrix[n][capacity];
+
+}
+
+int main(){
+
+	int price[] = {60, 100, 120};
+	int weight[] = {10, 20, 30};
+	int  capacity = 50;
+	int n = sizeof(price)/sizeof(price[0]);
+	printf("%d", knapSack(capacity, weight, price, n));
+	return 0;
 }
