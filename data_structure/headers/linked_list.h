ad2e1e35f3d31e5b8c10549a6c68bb05b80bccc4
meSajied <sajiedshahyousuf@gmail.com>
2020-10-26 21:13:19 +0600

    added: linked list

---
+++ data_structure/headers/linked_list.h
@@ -0,0 +1,26 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+class LinkedList{
+private:
+    int head;
+
+    class Node{
+        Node* next = NULL;
+        int data;
+
+        Node(int data){
+            this->data = data;
+        }
+    };
+
+public:
+    int create_head(int data);
+    int append_list(int data);
+    int delete_data(int data);
+    bool search(int data);
+    bool search(int first_node, int second_node);
+    int print_list();
+};
+
+#endif
---
+++ data_structure/linked_list.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "headers/linked_list.h"
+using namespace std;
+
+LinkedList::create_head(int data){
+    Node new_node(data);
+    new_node->next = this->head;
+    this->head = new_node;
+}
+
+LinkedList::append_list(int data){
+    Node* new_node(data);
+    Node* last= this->head;
+
+    new_node->next = NULL;
+
+    if(this->head == null){
+        this->head = data;
+        return;
+    }
+
+    while(last->next != NULL){
+        last = last->next;
+    }
+
+    last->next = new_node;
+
+    free(new_node);
+    free(last);
+
+}
+
+LinkedList::search(Node* node=this, int data){
+    if(node == NULL){
+        return NULL;
+    }
+
+    if(node->data == data){
+        return true;
+    }
+    else{
+        return search(node->next, x);
+    }
+}
+
+LinkedList::print_list(){
+    node *current=start;
+
+    while(current != NULL){
+        printf("%d ",current->data);
+        current=current->next;
+    }
+    printf("\n");
+}
